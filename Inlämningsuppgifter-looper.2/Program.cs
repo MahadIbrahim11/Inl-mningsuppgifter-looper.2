aa633f4fa8eb8d16d280080575124a5a5c757be6
maib07001 <maib07001@E-PW05C37V>
2024-12-13 08:36:33 +0100

    Add project files.

---
+++ Inlämningsuppgifter-looper.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace inl_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxtal = int.MinValue;
+            bool fortsätt = true;
+
+            while (fortsätt)
+            {
+                Console.WriteLine("skriv in ett heltal:");
+                int tal = int.Parse(Console.ReadLine());
+
+                if ( tal > maxtal )
+                {
+                    maxtal = tal;
+                }
+                Console.WriteLine("vill du skriva in ett tal till? (ja/nej)");
+                string svar = Console.ReadLine().ToLower();
+
+                if (svar != "ja")
+                {
+                    fortsätt = false;
+                }
+            }
+            Console.WriteLine($"det största talet du skrev in var: {maxtal}");
+            Console.ReadKey();
+        }
+    }
+}
